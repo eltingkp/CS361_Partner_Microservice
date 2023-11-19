e5932c12dc8a485567b7c3ed99b292cc2beb36b0
eltingkp <eltingk@oregonstate.edu>
2023-11-19 15:49:10 -0800

    Update a9_client.py

--- a9_client.py
+++ a9_client.py
@@ -5,7 +5,7 @@ client = socket.socket()
 
 client.connect(('localhost', 3000))
 
-movie_object = {"data": "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"}
+movie_object = {"data": "https://api.themoviedb.org/3/search/movie?query=comedy&include_adult=false&language=en-US&page=1&api_key=ENTER_YOUR_API_KEY"}
 
 json_string = json.dumps(movie_object)
 
